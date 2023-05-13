f38d306083e3f1e9571707d1b09ef6ed8d4e545c
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-13 15:09:39 -0500

    feat: update structure project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './styles/globals.scss'
+import App from './pages/index'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
