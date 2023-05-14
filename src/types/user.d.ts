6226de5b04d357d01b24888ef2aeab1b7c208d87
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-14 12:44:04 -0500

    feat: update structure user types

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,15 +1,43 @@
 import { UserCredential } from 'firebase/auth'
 import { Dispatch, SetStateAction } from 'react'
 
+export interface User {
+  uid: string
+  name?: string
+  phone?: string
+  gender?: string
+  country?: string
+  address?: string
+  zipCode?: string
+  lastName?: string
+  cellphone?: string
+  birthDate?: string
+  documentType?: string
+  documentNumber?: string
+}
+
+export interface UserValuesType {
+  email: string
+  password: string
+  remember?: boolean
+  confirmPassword?: string
+}
+
 export interface UserContextType {
   user: Record<string, unknown>
-  credentials: Record<string, unknown> | null
+  credentials: Record<string, string>
   logout: () => Promise<void>
-  signInWithGoogle: () => Promise<UserCredential | void>
+  signInWithGoogle: () => Promise<UserCredential | void | Error>
   setUser: Dispatch<SetStateAction<Record<string, unknown>>>
-  setCredentials: Dispatch<SetStateAction<Record<string, unknown>>>
-  signUp: (email: string, password: string) => Promise<UserCredential | void>
-  signIn: (email: string, password: string) => Promise<UserCredential | void>
+  setCredentials: Dispatch<SetStateAction<Record<string, string>>>
+  signUp: (
+    email: string,
+    password: string
+  ) => Promise<UserCredential | void | Error>
+  signIn: (
+    email: string,
+    password: string
+  ) => Promise<UserCredential | void | Error>
 }
 
 export interface UserProviderType {
