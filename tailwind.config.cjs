6e9f2984e3e884218d6d0caab4dc192dbeb862ed
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-14 00:35:28 -0500

    feat: update structure tailwind css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const withMT = require('@material-tailwind/react/utils/withMT')
+// eslint-disable-next-line no-undef
+module.exports = withMT({
   content: ['src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      flex: {
+        '1-auto': '1 0 auto'
+      },
+      colors: {
+        'sky-red': {
+          90: '#FF7885'
+        },
+        'sky-gray': {
+          50: '#94969A',
+          40: '#EAEBF7',
+          30: '#C0C3D0'
+        },
+        'sky-blue': {
+          90: '#553BF1',
+          80: '#4A3AFF',
+          70: '#5B86E5',
+          50: '#5b86e582'
+        }
+      },
+      boxShadow: {
+        'sky-buttons': '0px 15px 50px rgba(222, 95, 143, 0.12)'
+      }
+    }
   },
   plugins: []
-}
+})
