1a742fe1ce0cd111fb2aeb316507b95734287e93
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-14 12:44:52 -0500

    feat: implement methods firestore in context

--- src/context/user.tsx
+++ src/context/user.tsx
@@ -8,6 +8,7 @@ import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword
 } from 'firebase/auth'
+import { getUser, saveUser, verifyExistUser } from '../utils/firebaseFunctions'
 
 export const UserContext = createContext<UserContextType>({
   user: {},
@@ -24,21 +25,39 @@ export const UserProvider = ({ children }: UserProviderType) => {
   const [user, setUser] = useState({})
   const [credentials, setCredentials] = useState({})
 
-  const signIn = (email: string, password: string) => {
-    return signInWithEmailAndPassword(auth, email, password)
+  const signIn = async (email: string, password: string) => {
+    const currentUser = await signInWithEmailAndPassword(auth, email, password)
+    if (!currentUser) {
+      return Error('No se pudo iniciar sesión')
+    }
+    const { user } = currentUser
+    getUser(user.uid)
   }
 
-  const signUp = (email: string, password: string) => {
-    return createUserWithEmailAndPassword(auth, email, password)
+  const signUp = async (email: string, password: string) => {
+    const newUser = await createUserWithEmailAndPassword(auth, email, password)
+    if (!newUser) {
+      return Error('No se pudo crear el usuario')
+    }
+    const { user } = newUser
+    saveUser({
+      uid: user.uid
+    })
   }
 
   const logout = () => {
     return auth.signOut()
   }
 
-  const signInWithGoogle = () => {
+  const signInWithGoogle = async () => {
     const googleProvider = new GoogleAuthProvider()
-    return signInWithPopup(auth, googleProvider)
+    const currentUser = await signInWithPopup(auth, googleProvider)
+    if (!currentUser) return Error('No se pudo iniciar sesión')
+    const { user } = currentUser
+    verifyExistUser(user.uid).then(data => {
+      if (!data) saveUser({ uid: user.uid })
+      else getUser(user.uid)
+    })
   }
 
   useEffect(() => {
