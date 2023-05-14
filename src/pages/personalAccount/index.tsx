cc32816ebd2540de4a44803f3543d172387dc215
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-14 12:45:22 -0500

    feat: implement stepper material tailwind with personal account

--- src/pages/personalAccount/index.tsx
+++ src/pages/personalAccount/index.tsx
@@ -1,5 +1,37 @@
+import { useState } from 'react'
+import { Account } from '../../components/account'
+import { Location } from '../../components/location'
+import { Personal } from '../../components/personal'
+import { Stepper, Step } from '@material-tailwind/react'
+import { HomeIcon, CogIcon, UserIcon } from '@heroicons/react/24/outline'
+
 const PersonalAccount = () => {
-  return <div>Personal</div>
+  const [stepper, setStepper] = useState(0)
+
+  return (
+    <main className='w-full h-full flex flex-col overflow-x-hidden items-center'>
+      <div className='flex flex-col xl:flex-row justify-center items-center w-full py-3 px-4 sm:pl-8 sm:pr-4 max-w-[1440px] gap-4 flex-1-auto m-0-auto'>
+        <div className='flex flex-col gap-4 w-full h-full'>
+          <Stepper activeStep={stepper}>
+            <Step onClick={() => setStepper(0)}>
+              <HomeIcon className='h-5 w-5' />
+            </Step>
+            <Step onClick={() => setStepper(1)}>
+              <UserIcon className='h-5 w-5' />
+            </Step>
+            <Step onClick={() => setStepper(2)}>
+              <CogIcon className='h-5 w-5' />
+            </Step>
+          </Stepper>
+          <div className='bg-white w-full border solid rounded-2xl p-4 flex '>
+            {stepper === 2 && <Location />}
+            {stepper === 1 && <Account />}
+            {stepper === 0 && <Personal />}
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default PersonalAccount
