8c1676efc37c5b3e4a577cb2a5a6b48f9927503f
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-15 03:45:14 -0500

    feat: update rules eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,8 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     'react-refresh/only-export-components': 'warn'
   }
