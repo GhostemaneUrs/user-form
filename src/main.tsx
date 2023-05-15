1ffc1b8611151740aca1005f27fd1b406924eff1
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-15 03:45:04 -0500

    feat: implement context

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,21 @@ import React from 'react'
 import './styles/globals.scss'
 import App from './pages/index'
 import { createRoot } from 'react-dom/client'
-import { UserProvider } from './context/user'
+import { UserProvider } from './context/auth'
 import { BrowserRouter } from 'react-router-dom'
+import { StepsProvider } from './context/steps'
 import { ThemeProvider } from '@material-tailwind/react'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <UserProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <StepsProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </StepsProvider>
     </UserProvider>
   </React.StrictMode>
 )
