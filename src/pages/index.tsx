828d35452b385fc76d1c91cc711486209aa6ba90
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-15 03:44:55 -0500

    feat: implement protected routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import SignIn from './signIn'
 import SignUp from './signUp'
-import { useEffect } from 'react'
+import { Fragment, useEffect } from 'react'
+import Personal from './personalAccount'
 import { Layout } from '../components/layout'
+import {
+  ProtectedRoute,
+  ProtectedRouteAuth
+} from '../components/protectedRoute'
+import { useAuth } from '../hooks/useAuth'
+import { Loading } from '../components/loading'
 import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
+
 const AppRouter = () => {
+  const { loading } = useAuth()
   const location = useLocation()
   const navigate = useNavigate()
   const loggedOutRoutes = ['/sign-in']
@@ -15,13 +24,21 @@ const AppRouter = () => {
   }, [])
 
   return (
-    <Layout>
-      <Routes>
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='*' element={<h1>404</h1>} />
-      </Routes>
-    </Layout>
+    <Fragment>
+      <Loading state={loading} />
+      <Layout>
+        <Routes>
+          <Route element={<ProtectedRouteAuth />}>
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path='/personal-account' element={<Personal />} />
+          </Route>
+          <Route path='*' element={<h1>404</h1>} />
+        </Routes>
+      </Layout>
+    </Fragment>
   )
 }
 
