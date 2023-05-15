cff0cb071632b5ec1ee0ca124449d12753710887
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-05-15 03:39:48 -0500

    feat: update structure layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,17 @@
+import { useLocation } from 'react-router-dom'
+import { Header } from '../header'
+
 interface LayoutProps {
   children: React.ReactNode
 }
 
 export const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation()
+  const { pathname } = location
   return (
-    <main className='w-full flex justify-center items-center min-h-screen bg-[#F6F7FB]'>
-      {children}
-    </main>
+    <div className='flex flex-col min-h-screen w-full'>
+      {pathname !== '/sign-in' && pathname !== '/sign-up' && <Header />}
+      <main className='w-full flex flex-1-auto'>{children}</main>
+    </div>
   )
 }
